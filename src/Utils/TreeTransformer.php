6c1591b7a5b2f5ec038883e1c1d6bf73254f37f3
danielzhang130 <danielzhang130@gmail.com>
2023-02-03 22:47:15 -0500

    api update

--- src/Utils/TreeTransformer.php
+++ src/Utils/TreeTransformer.php
@@ -6,20 +6,36 @@ class TreeTransformer
 {
     public function toTree($merchants): array
     {
+        $category_map = [];
+        $sub_category_map = [];
         $categories = [];
+
         foreach ($merchants as $entry) {
             $subCategory = $entry->subCategory->name;
             $category = $entry->subCategory->category->name;
-            if (!isset($categories[$category])) {
-                $categories[$category] = [];
+
+            if (!isset($category_map[$category])) {
+                $new_category = [
+                    "name" => $category,
+                    "subcategories" => []
+                ];
+                $category_map[$category] = sizeof($categories);
+                $categories[] = $new_category;
             }
-            if (!isset($categories[$category][$subCategory])) {
-                $categories[$category][$subCategory] = [];
+
+            if (!isset($sub_category_map[$subCategory])) {
+                $new_sub_category = [
+                    "name" => $subCategory,
+                    "merchants" => []
+                ];
+                $sub_category_map[$subCategory] = sizeof($categories[$category_map[$category]]["subcategories"]);
+                $categories[$category_map[$category]]["subcategories"][] = $new_sub_category;
             }
-            if (in_array($entry->name, $categories[$category][$subCategory])) {
+
+            if (in_array($entry->name, $categories[$category_map[$category]]["subcategories"][$sub_category_map[$subCategory]]["merchants"])) {
                 continue;
             }
-            $categories[$category][$subCategory][] = $entry->name;
+            $categories[$category_map[$category]]["subcategories"][$sub_category_map[$subCategory]]["merchants"][] = $entry->name;
         }
 
         return $categories;
